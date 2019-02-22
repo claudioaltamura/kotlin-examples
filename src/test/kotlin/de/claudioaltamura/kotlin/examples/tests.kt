b26cd8d527e9ecf9537b6cfbdcf8bffc38b6454d
Claudio Altamura <claudio@claudioaltamura.de>
2019-02-22 09:34:49 +0100

    new: inital commit

---
+++ src/test/kotlin/de/claudioaltamura/kotlin/examples/tests.kt
@@ -0,0 +1,10 @@
+package de.claudioaltamura.kotlin.examples
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class TestSource {
+    @Test fun testGreeting() {
+        assertEquals("Hello, world!", getGreeting())
+    }
+}
