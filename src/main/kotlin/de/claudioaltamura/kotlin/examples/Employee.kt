c0f4716e82d3aa125ab41220ff8c373bc54bc1d9
Claudio Altamura <claudio@claudioaltamura.de>
2019-06-14 09:57:43 +0200

    new: equals and hashCode

--- src/main/kotlin/de/claudioaltamura/kotlin/examples/Employee.kt
+++ src/main/kotlin/de/claudioaltamura/kotlin/examples/Employee.kt
@@ -2,5 +2,14 @@ package de.claudioaltamura.kotlin.examples
 
 class Employee(var name: String, val id : Int) {
 
+    override fun equals(other: Any?): Boolean {
+        if(other == null || other !is Employee)
+            return false
+        return id == other.id && name==other.name
+    }
+
+    override fun hashCode(): Int =
+            31+id * name.hashCode()
+
     override fun toString() : String = name + "," + id
 }
---
+++ src/test/kotlin/de/claudioaltamura/kotlin/examples/EmployeeEqualsTest.kt
@@ -0,0 +1,32 @@
+package de.claudioaltamura.kotlin.examples
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Test
+
+class EmployeeEqualsTest {
+
+  @Test
+  fun testEquals() {
+    val employee1 = Employee("Tom", 500)
+    val employee2 = Employee("Tom", 500)
+    val employee3 = Employee("Peter", 501)
+
+    println("==")
+    println(employee1==employee2)
+    println(employee1==employee3)
+
+    println("equals")
+    println(employee1.equals(employee2))
+    println(employee1.equals(employee3))
+
+    assertEquals(employee1, employee2)
+    assertNotEquals(employee1, employee3)
+
+    println("===")
+    println(employee1===employee2)
+    println(employee1===employee3)
+
+  }
+
+}
