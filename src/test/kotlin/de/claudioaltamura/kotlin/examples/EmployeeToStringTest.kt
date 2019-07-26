0b2cfa0e013d6e61c72e7cb1dbfef6312a3adcbd
Claudio Altamura <claudio@claudioaltamura.de>
2019-07-26 17:10:10 +0200

    fix: broken test

--- src/test/kotlin/de/claudioaltamura/kotlin/examples/EmployeeToStringTest.kt
+++ src/test/kotlin/de/claudioaltamura/kotlin/examples/EmployeeToStringTest.kt
@@ -9,7 +9,7 @@ class EmployeeToStringTest {
   fun testToString() {
     val employee1 = Employee("Tom", 500)
 
-    assertEquals(employee1.toString(), "Tom,500")
+    assertEquals(employee1.toString(), "Employee(name='Tom', id=500)")
   }
 
 }
