eecad8f2866d9812b331313ac2741376f1016eff
Claudio Altamura <claudio@claudioaltamura.de>
2020-01-08 09:05:29 +0100

    new: for example

---
+++ src/main/kotlin/de/claudioaltamura/kotlin/examples/ForExample.kt
@@ -0,0 +1,10 @@
+package de.claudioaltamura.kotlin.examples
+
+fun main(args: Array<String>)  {
+
+  val elements = arrayWithLambdaExpression();
+
+  for(element in elements) {
+    println(element)
+  }
+}
