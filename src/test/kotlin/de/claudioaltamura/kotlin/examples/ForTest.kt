e831f6abfadb2894a67c77ee8cdc8b71fae8a6b5
Claudio Altamura <claudio@claudioaltamura.de>
2020-10-07 09:27:13 +0200

    chg: added more for examples

--- src/test/kotlin/de/claudioaltamura/kotlin/examples/ForTest.kt
+++ src/test/kotlin/de/claudioaltamura/kotlin/examples/ForTest.kt
@@ -18,4 +18,51 @@ class ForTest {
     assertTrue('q' in charRange)
   }
 
+  @Test
+  fun testRangeDownTo() {
+    val range = 5.downTo(1);
+
+    var result = ""
+    for(i in range) {
+      result += i
+    }
+
+    assertEquals("54321", result)
+  }
+
+
+  @Test
+  fun testRangeStepped() {
+    val range = 1..5
+
+    var result = ""
+    for(i in range.step(2)) {
+      result += i
+    }
+
+    assertEquals("135", result)
+  }
+
+  @Test
+  fun testRangeStepAlternative() {
+    var result = ""
+    for(num in 1..20 step 4) {
+      result += num
+    }
+    assertEquals("1591317" ,result);
+  }
+
+
+  @Test
+  fun testCharRangeReversed() {
+    val charRange = 'a'..'c'
+
+    var result = ""
+    for(i in charRange.reversed()) {
+      result += i
+    }
+
+    assertEquals("cba", result)
+  }
+
 }
