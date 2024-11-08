1782dc03325810966cdb1c556db4449734cc7e93
claudioaltamura <claudio@claudioaltamura.de>
2024-11-08 08:41:43 +0100

    test: added a when test

---
+++ src/main/kotlin/de/claudioaltamura/kotlin/examples/OrdinalUtils.kt
@@ -0,0 +1,9 @@
+package de.claudioaltamura.kotlin.examples
+
+fun getOrdinal(n: Int) = when {
+    n in 11..13 -> "${n}th"
+    n % 10 == 1 -> "${n}st"
+    n % 10 == 2 -> "${n}nd"
+    n % 10 == 3 -> "${n}rd"
+    else -> "${n}th"
+}
--- src/test/kotlin/de/claudioaltamura/kotlin/examples/WhenTest.kt
+++ src/test/kotlin/de/claudioaltamura/kotlin/examples/WhenTest.kt
@@ -2,12 +2,16 @@ package de.claudioaltamura.kotlin.examples
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class WhenTest {
 
   @Test
   fun testWhen() {
-    val num = 100;
+    val num = 100
     val secondNum = when {
       num > 50 -> 1
       num < 20 -> -1
@@ -21,7 +25,7 @@ class WhenTest {
   fun testWhenIs() {
     val something: Any = "12"
 
-    val result = when(something) {
+    val result = when (something) {
       is String -> "String"
       is Int -> "Integer"
       else -> "Unknown"
@@ -32,9 +36,9 @@ class WhenTest {
 
   @Test
   fun testWhenWithRanges() {
-    val num = 50;
+    val num = 50
 
-    val result = when(num) {
+    val result = when (num) {
       in 0..99 -> Ranges.ZERO_99
       in 100..199 -> Ranges.HUNDRED_199
       else -> Ranges.ABOVE
@@ -47,6 +51,22 @@ class WhenTest {
     ZERO_99,
     HUNDRED_199,
     ABOVE
+  }
+
+  @ParameterizedTest
+  @MethodSource("numbers")
+  fun testWhenWithRanges(number: Int, result: String) {
+    assertEquals(result, getOrdinal(number))
+  }
+
+  companion object {
+    @JvmStatic
+    fun numbers(): Stream<Arguments> {
+      return Stream.of(
+        Arguments.arguments(1, "1st"),
+        Arguments.arguments(2, "2nd")
+      )
+    }
   }
 
 }
